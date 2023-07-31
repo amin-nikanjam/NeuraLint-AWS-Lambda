6dc20c8517ecacd06afca52e115ac32d10b5eee5
Ghassen-Da <ghassendaoud99@gmail.com>
2023-07-31 15:36:19 -0400

    commit

--- endToEnd.py
+++ endToEnd.py
@@ -60,7 +60,7 @@ def parsDnnScript(fileName, parser = "tf" , inputSize=[128, 20], outputSize=[128
                 f'java -jar {groovePath}Generator.jar -f {grooveOutputFileName}.gst -s bfs {grammarName}.gps {FileNameWithoutPath}.gst')
         except:
             return f"{FileNameWithoutPath}.py\n\rError: input file is not valid or not match with selected parser type"
- 
+
 
     try:
         result = f"{FileNameWithoutPath}.py\n\r" + grooveParser.main(grooveOutputFileName, parserType = parser)
